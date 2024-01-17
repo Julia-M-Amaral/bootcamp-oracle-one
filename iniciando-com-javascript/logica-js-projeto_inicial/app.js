16cb9348ad33b205150249f6e85753c0079a8276
Julia "jubsz <JuliaAmaralSantos@outlook.com>
2024-01-17 16:12:49 -0300

    adicionei looping para o código continuar executando caso o usuário não acerte na primeira tentativa.

--- iniciando-com-javascript/logica-js-projeto_inicial/app.js
+++ iniciando-com-javascript/logica-js-projeto_inicial/app.js
@@ -1,9 +1,20 @@
 //alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 4;
+let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
+let tentativas = 1;
 
+while(numeroSecreto != chute){
+    chute = prompt('Escolha um número entre 1 e 10');
+    if(numeroSecreto == chute){
+        alert(`Parabéns! Você acertou o número secreto: ${numeroSecreto}. Número de tentativas: ${tentativas}`);
+    } else {
+        if (numeroSecreto > chute) {
+            alert(`O número secreto é maior que ${chute}`);
+        } else {
+            alert(`O número secreto é menor que ${chute}`);
+        }
+        tentativas++;
+    }
+}
 
-if(numeroSecreto == chute){
-    alert(`Parabéns! Você acertou o número secreto: ${numeroSecreto}.`);
-} else alert(`Errou! O número secreto é: ${numeroSecreto}`);
