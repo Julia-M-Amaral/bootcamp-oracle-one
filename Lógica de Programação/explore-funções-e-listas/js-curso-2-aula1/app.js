87c6a7f7c8c0f2324d3d5cfff81056f1ffa54cb0
Julia Mangabeira <juliaamaralsantos@outlook.com>
2024-01-25 11:19:26 -0300

    construindo jogo do numero secreto manipulando o conteudo com dom

---
+++ Lógica de Programação/explore-funções-e-listas/js-curso-2-aula1/app.js
@@ -0,0 +1,31 @@
+
+let numeroSecreto = gerarNumeroAleatorio();
+console.log(numeroSecreto);
+
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = (texto);
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+  let chute = document.querySelector('input').value;
+
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela('h1', 'Acertou!')
+    exibirTextoNaTela('p', `Você descobriu o número secreto - ${numeroSecreto}`)
+  } else {
+    if (chute > numeroSecreto) {
+      console.log()
+      exibirTextoNaTela('p', 'O número secreto é menor!')
+    } else {
+      exibirTextoNaTela('p', 'O número secreto é maior!')
+    }
+  }
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
+}
