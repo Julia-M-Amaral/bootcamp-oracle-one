236cbf37cf591971ac80f84f3a084332a76037a7
Julia Mangabeira <juliaamaralsantos@outlook.com>
2024-01-25 14:05:39 -0300

    função que limpa o input após inserir um número

--- Lógica de Programação/explore-funções-e-listas/js-curso-2-aula1/app.js
+++ Lógica de Programação/explore-funções-e-listas/js-curso-2-aula1/app.js
@@ -1,5 +1,5 @@
-
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 console.log(numeroSecreto);
 
 function exibirTextoNaTela(tag, texto) {
@@ -14,8 +14,13 @@ function verificarChute() {
   let chute = document.querySelector('input').value;
 
   if (chute == numeroSecreto) {
+
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagemTentativa = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}.`
+
     exibirTextoNaTela('h1', 'Acertou!')
-    exibirTextoNaTela('p', `Você descobriu o número secreto - ${numeroSecreto}`)
+    exibirTextoNaTela('p', mensagemTentativa)
+
   } else {
     if (chute > numeroSecreto) {
       console.log()
@@ -23,9 +28,16 @@ function verificarChute() {
     } else {
       exibirTextoNaTela('p', 'O número secreto é maior!')
     }
+    tentativas++;
+    limparCampo()
   }
 }
 
 function gerarNumeroAleatorio() {
   return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo(){
+  chute = document.querySelector('input');
+  chute.value = '';
 }
